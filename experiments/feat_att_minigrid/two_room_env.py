b454d2fcdb4e2211fca262563e27a3c964beee6f
Kevin Roice <roice@ualberta.ca>
2023-11-02 21:46:13 -0600

    feat: added 4rooms

--- experiments/feat_att_minigrid/two_room_env.py
+++ experiments/feat_att_minigrid/two_room_env.py
@@ -68,8 +68,81 @@ class TwoRoomEnv(MiniGridEnv):
 
         self.mission = "two-room"
 
+class FourRoomEnv(MiniGridEnv):
+    def __init__(
+        self,
+        width=13,
+        height=13,
+        agent_start_pos=(1, 1),
+        agent_start_dir=0,
+        hallway_pos=[(3,6), (6, 2), (7, 9), (10, 6)],
+        max_steps: int = None,
+        **kwargs,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+        self.hallway_pos = hallway_pos
+
+        mission_space = MissionSpace(mission_func=self._gen_mission)
+
+        super().__init__(
+            mission_space=mission_space,
+            width=width,
+            height=height,
+            max_steps=1000,
+            **kwargs,
+        )
+
+    @staticmethod
+    def _gen_mission():
+        return "grand mission"
+    
+    # MiniGridEnv._gen_grid
+    @abstractmethod
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Generate verical separation wall
+        for i in range(1, 3):
+            self.grid.set(6, i, Wall())
+
+        for i in range(4, 10):
+            self.grid.set(6, i, Wall())
+
+        for i in range(11, 12):
+            self.grid.set(6, i, Wall())
+
+        # Generate horizontal separation wall
+        for i in range(1, 2):
+            self.grid.set(i, 6, Wall())
+        
+        for i in range(3, 6):
+            self.grid.set(i, 6, Wall())
+
+        for i in range(7, 9):
+            self.grid.set(i, 7, Wall())
+
+        for i in range(10, 12):
+            self.grid.set(i, 7, Wall())
+
+        # Place a goal square in the bottom-right corner
+        self.put_obj(Goal(), width - 2, height - 2)
+
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.agent_pos = self.agent_start_pos
+            self.agent_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "four-room"
+
 def main():
-    env = TwoRoomEnv(render_mode="human")
+    env = FourRoomEnv(render_mode="human")
 
     # enable manual control for testing
     manual_control = ManualControl(env, seed=42)
