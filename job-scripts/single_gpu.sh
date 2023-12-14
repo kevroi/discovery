684dd429f3abe4919140003c2cd66bf8501e9110
Kevin Roice <roice@cedar5.cedar.computecanada.ca>
2023-12-13 22:37:51 -0800

    chore: tided shell script and added model

--- job-scripts/single_gpu.sh
+++ job-scripts/single_gpu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -J PPO_DK8_RGB
+#SBATCH --job-name=PPO_DK8_RGB
 #SBATCH --account=rrg-whitem
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=0-2:59
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
+#SBATCH --time=0-2:59
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=roice@ualberta.ca
-#SBATCH -o /home/roice/scratch/discovery/logs/PPO_DK8_RGB.out # Standard output
-#SBATCH -e /home/roice/scratch/discovery/logs/PPO_DK8_RGB.err # Standard error
+#SBATCH -o /home/roice/scratch/discovery/logs/%x.out
+#SBATCH -e /home/roice/scratch/discovery/logs/%x.err
 
 module load python/3.9
 source venv/bin/activate
