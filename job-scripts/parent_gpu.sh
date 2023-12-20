52d80b5ea89fea4301434d482d5250026d58fcd8
Kevin Roice <roice@ualberta.ca>
2023-12-20 03:20:22 -0700

    chore: bash syntax fixes

--- job-scripts/parent_gpu.sh
+++ job-scripts/parent_gpu.sh
@@ -11,14 +11,15 @@
 #SBATCH -e /home/roice/scratch/discovery/logs/%x.err
 
 # Define your parameter sweep values
-env_name = 'MiniGrid-Empty-8x8-v0' 
-lrs = (0.000001 0.00001 0.0001 0.001 0.01 0.1)
-num_runs = 10
+env_name='MiniGrid-Empty-8x8-v0' 
+lrs=(0.1)
+num_runs=10
 
 # Loop over the parameter sweep values
 for lr in ${lrs[@]}; do
-    for i in $(seq $(num_runs)); do
+    for i in $(seq 1 $num_runs); do
         # Submit a job for each parameter combination
-        sbatch --export=ALL,env_name="$env_name",lr="$lr" job-scripts/child_gpu.sh
+        # sbatch --export=ALL,env_name="$env_name",lr="$lr" job-scripts/child_gpu.sh
+        echo "sbatch --export=ALL,env_name=$env_name,lr=$lr job-scripts/child_gpu.sh"
     done
 done
