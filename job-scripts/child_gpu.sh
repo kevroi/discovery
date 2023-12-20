1e1e8863d725f32fd22f5eb3d45009e8bb477746
Kevin Roice <roice@ualberta.ca>
2023-12-20 03:50:56 -0700

    fixing parent scripts

--- job-scripts/child_gpu.sh
+++ job-scripts/child_gpu.sh
@@ -11,14 +11,11 @@
 #SBATCH -o /home/roice/scratch/discovery/logs/%j.out
 #SBATCH -e /home/roice/scratch/discovery/logs/%j.err
 
-# Extract environment variables passed from the main script
-env_name=$1
-lr=$2
-
+echo $ENV_NAME, $LR
 
 module load python/3.9
 source venv/bin/activate
 pip list
 wandb login
 
-python $me/discovery/experiments/FeatAct_minigrid/run_minigrid.py --env_name=$env_name --lr=$lr
+python $me/discovery/experiments/FeatAct_minigrid/run_minigrid.py --env_name=$ENV_NAME --lr=$LR
