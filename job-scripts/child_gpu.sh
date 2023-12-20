154490e2a39fcf110d4468a4dbf7ab0d0f779ac3
Kevin Roice <roice@ualberta.ca>
2023-12-20 03:07:07 -0700

    feat:sending multiple jobs

---
+++ job-scripts/child_gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=rrg-whitem
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=0-2:59
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=roice@ualberta.ca
+
+# Extract environment variables passed from the main script
+env_name=$1
+lr=$2
+
+#SBATCH --job-name=PPO_${env_name}_lr${lr}
+#SBATCH -o /home/roice/scratch/discovery/logs/%x.out
+#SBATCH -e /home/roice/scratch/discovery/logs/%x.err
+
+module load python/3.9
+source venv/bin/activate
+pip list
+wandb login
+
+python $me/discovery/experiments/FeatAct_minigrid/run_minigrid.py --env_name=$env_name --lr=$lr
---
+++ job-scripts/parent_gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=parent_job
+#SBATCH --account=rrg-whitem
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=0-2:59
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=roice@ualberta.ca
+#SBATCH -o /home/roice/scratch/discovery/logs/%x.out
+#SBATCH -e /home/roice/scratch/discovery/logs/%x.err
+
+# Define your parameter sweep values
+env_name = 'MiniGrid-Empty-8x8-v0' 
+lrs = (0.000001 0.00001 0.0001 0.001 0.01 0.1)
+num_runs = 10
+
+# Loop over the parameter sweep values
+for lr in ${lrs[@]}; do
+    for i in $(seq $(num_runs)); do
+        # Submit a job for each parameter combination
+        sbatch --export=ALL,env_name="$env_name",lr="$lr" job-scripts/child_gpu.sh
+    done
+done
