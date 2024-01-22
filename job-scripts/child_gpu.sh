9ddf0b114cf54b3501b822a1654d6f7b93ad7ce7
Kevin Roice <roice@cedar5.cedar.computecanada.ca>
2024-01-21 16:21:20 -0800

    updated job scripts

--- job-scripts/child_gpu.sh
+++ job-scripts/child_gpu.sh
@@ -5,17 +5,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=0-2:59
+#SBATCH --time=0-2:55
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=roice@ualberta.ca
-#SBATCH -o /home/roice/scratch/discovery/logs/%j.out
-#SBATCH -e /home/roice/scratch/discovery/logs/%j.err
+
 
 echo $ENV_NAME, $LR
 
 module load python/3.9
 source venv/bin/activate
-pip list
 wandb login
 
-python $me/discovery/experiments/FeatAct_minigrid/run_minigrid.py --env_name=$ENV_NAME --lr=$LR --use_wandb
+# python $me/discovery/experiments/FeatAct_minigrid/run_minigrid.py --env_name=$ENV_NAME --lr=$LR --use_wandb
+python $me/discovery/experiments/FeatAct_atari/run_atari.py --env_name=$ENV_NAME --lr=$LR --use_wandb
