a91ebf927586dd1043a5ad3a49114df85c9113b3
Kevin Roice <roice@ualberta.ca>
2024-04-07 19:16:14 -0600

    feat: wandb downloader and plotter

--- experiments/FeatAct_minigrid/wandb_downloader.py
+++ experiments/FeatAct_minigrid/wandb_downloader.py
@@ -12,7 +12,7 @@ filter_dict = {
     "$and": [
     {'config.env_name': "MiniGrid-DoorKey-5x5-v0"},
     {'config.learner': "PPO"},
-    {'config.activation': "relu"},
+    {'config.activation': "crelu"},
     {'config.feat_dim': 40},
     # {"summary.rollout/ep_rew_mean": {"$gt": 0.95}}
     ]
@@ -23,5 +23,6 @@ runs_df = pd.DataFrame()
 for run in runs:
     runs_df[run.id] = run.history()[metric]
 
+runs_df = runs_df.fillna(0.0)
 # save to csv
 runs_df.to_csv(f"experiments/FeatAct_minigrid/wandb_export_data/{project}_{filter_dict['$and'][0]['config.env_name']}_{filter_dict['$and'][1]['config.learner']}_{filter_dict['$and'][2]['config.activation']}_{filter_dict['$and'][3]['config.feat_dim']}_data.csv")
