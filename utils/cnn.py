e762d7d424c2959d266976bfbb35a106e7bbdd92
Kevin Roice <roice@ualberta.ca>
2024-04-15 16:44:28 -0600

    chore: moved utils, added SharedPriv architecture

---
+++ utils/cnn.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import gymnasium as gym
+from gymnasium import spaces
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.preprocessing import is_image_space
+
+from utils.activations import CReLU, FTA
+
+# CNN from MiniGrid Documentation
+class MinigridFeaturesExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.Space, features_dim: int = 512,
+                 normalized_image: bool = False, last_layer_activation="relu") -> None:
+        super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 16, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (2, 2)),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+
+        if last_layer_activation == "relu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+        elif last_layer_activation == "crelu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim//2), CReLU())
+        elif last_layer_activation == "fta":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim//20), FTA())
+        elif last_layer_activation == "lrelu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.LeakyReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
+    
+
+class SharedPrivateFeaturesExtractor(MinigridFeaturesExtractor):
+    def __init__(self, observation_space: gym.Space, env: gym.Env,
+                 features_dim: int = 512, normalized_image: bool = False, last_layer_activation="relu") -> None:
+        self.env = env
+        self.num_shared = features_dim
+        self.num_variants = env.num_variants
+        super().__init__(observation_space, features_dim+self.num_variants, normalized_image, last_layer_activation)
+        # self.private_weights = nn.Parameter(torch.randn(self.num_variants))
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        features = super().forward(observations)
+        variant_idx = self.env.variant_idx  # Assuming env has a variant_idx attribute
+
+        for i in range(self.num_variants):
+            if i != variant_idx:
+                features[:, self.num_shared+i] = features[:, self.num_shared+i].detach()
+            else:
+                features[:, self.num_shared+i].requires_grad_()  # Reattach the ith variant
+
+        return features
+
+
+        
+# CNN from DQN Nature paper
+class NatureCNN(BaseFeaturesExtractor):
+    """
+    CNN from DQN Nature paper:
+        Mnih, Volodymyr, et al.
+        "Human-level control through deep reinforcement learning."
+        Nature 518.7540 (2015): 529-533.
+
+    :param observation_space:
+    :param features_dim: Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    :param normalized_image: Whether to assume that the image is already normalized
+        or not (this disables dtype and bounds checks): when True, it only checks that
+        the space is a Box and has 3 dimensions.
+        Otherwise, it checks that it has expected dtype (uint8) and bounds (values in [0, 255]).
+    """
+
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+        normalized_image: bool = False,
+        last_layer_activation="relu",
+    ) -> None:
+        assert isinstance(observation_space, spaces.Box), (
+            "NatureCNN must be used with a gym.spaces.Box ",
+            f"observation space, not {observation_space}",
+        )
+        super().__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+        assert is_image_space(observation_space, check_channels=False, normalized_image=normalized_image), (
+            "You should use NatureCNN "
+            f"only with images not with {observation_space}\n"
+            "(you are probably using `CnnPolicy` instead of `MlpPolicy` or `MultiInputPolicy`)\n"
+            "If you are using a custom environment,\n"
+            "please check it using our env checker:\n"
+            "https://stable-baselines3.readthedocs.io/en/master/common/env_checker.html.\n"
+            "If you are using `VecNormalize` or already normalized channel-first images "
+            "you should pass `normalize_images=False`: \n"
+            "https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html"
+        )
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+
+        if last_layer_activation == "relu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+        elif last_layer_activation == "crelu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim//2), CReLU())
+        elif last_layer_activation == "fta":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim//20), FTA())
+        elif last_layer_activation == "lrelu":
+            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.LeakyReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
