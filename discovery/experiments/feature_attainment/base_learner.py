75431d320d49059a61ed6010e0838dcba877a7fe
Kevin Roice <roice@ualberta.ca>
2024-05-08 18:11:07 -0600

    chore: updated old dirs

---
+++ discovery/experiments/feature_attainment/base_learner.py
@@ -0,0 +1,53 @@
+import sys
+import numpy as np
+import pickle
+import copy
+from tqdm import tqdm
+
+import utils.rlglue as rlglue
+from environments.environment import ToyEnvironment, GridEnvironment, I_MazeEnvironment
+from agents.TD_lambda import TDLambdaAgent
+from agents.Sarsa_lmbda import SarsaLmbdaAgent
+from eigenoptions.options import Options
+
+def see_action_values(Q):
+    action_set = ['→', '←', '↓', '↑']
+    print(np.round(np.max(Q, axis=2), decimals=1))
+    actions = np.argmax(Q, axis=2)
+    opt_acts = np.zeros(actions.shape, dtype=str)
+    for i in range(actions.shape[0]):
+        for j in range(actions.shape[1]):
+            opt_acts[i,j] = action_set[actions[i,j]]
+    print(opt_acts)
+
+# env = GridEnvironment()
+env = ToyEnvironment('2room_lava')
+max_row, max_col = env.get_grid_dimension()
+
+# Figure 2 Agent
+agent = SarsaLmbdaAgent(max_row=max_row, max_col=max_col)
+agent.set_alpha(0.1)
+agent.set_discount(0.9)
+agent.set_epsilon(0.1)
+agent.set_lmbda(0.9)
+glue = rlglue.RLGlue(env, agent)
+
+
+num_runs = 30
+num_episodes = 50
+cum_reward = np.zeros(num_episodes)
+
+for run in tqdm(range(num_runs)):
+    for ep in range(num_episodes):
+        # run episode
+        glue.episode(1000)
+        cum_reward[ep] += glue.get_total_reward()
+        glue.cleanup_episode()
+        agent.epsilon *= 0.99
+    learned_Q = agent.get_Q()
+    see_action_values(learned_Q)
+    glue.cleanup_run()
+cum_reward /= float(num_runs)
+
+np.save(f'experiments/feature_attainment/data_files/{env.name}_{agent.name}_average_return', cum_reward)
+
---
+++ discovery/experiments/feature_attainment/plot_v_subgoals.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+env_name = "grid"
+env_name = "2room_lava"
+agent_name = "Sarsa(lambda)"
+agent_name = "Sarsa(lambda)_FeatAtt"
+# env_name = "4room"
+
+# if __name__ == "__main__":
+results = np.load(f'experiments/feature_attainment/data_files/{env_name}_{agent_name}_hallway_vf.npy')
+print(np.shape(results))
+
+plt.show()
+x_legend = range(len(results[:]))
+graph_agent_0, = plt.plot(x_legend, results, label=agent_name)
+# graph_agent_2, = plt.plot(x_legend, results[2][:], label="2 option")
+# graph_agent_4, = plt.plot(x_legend, results[4][:], label="4 options")
+# graph_agent_8, = plt.plot(x_legend, results[8][:], label="8 options")
+# graph_agent_64, = plt.plot(x_legend, results[64][:], label="64 options")
+# graph_agent_128, = plt.plot(x_legend, results[128][:], label="128 options")
+# graph_agent_200, = plt.plot(x_legend, results[200][:], label="200 options")
+
+# plt.axhline(y=-18, color='purple', linestyle='--', label='Optimal')
+
+
+plt.legend(handles=[graph_agent_0])
+plt.xlabel('Episodes')
+plt.ylabel('Average return')
+plt.title(f'{env_name}')
+plt.show()
+# plt.savefig(f'experiments/average_return/plots/{env_name}_average_return.png', dpi=300)
