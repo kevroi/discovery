4d33b98887b6b95c6e5aba83b5aa6db3bac1edc1
David Szepesvari <szepesvari.david@gmail.com>
2024-05-14 17:30:14 -0600

    Update single_cpu bash script.

--- job-scripts/single_cpu.sh
+++ job-scripts/single_cpu.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=PPO_MontRev_WB
 #SBATCH --account=rrg-whitem
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
 #SBATCH --time=0-2:59
@@ -11,7 +10,7 @@
 #SBATCH -o /home/roice/scratch/discovery/logs/%x.out
 #SBATCH -e /home/roice/scratch/discovery/logs/%x.err
 
-module load python/3.9
+module load python/3.10
 source venv/bin/activate
 wandb login
 python $me/discovery/discovery/experiments/FeatAct_minigrid/run_minigrid.py
