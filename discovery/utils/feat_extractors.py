f00531588267da3292a9c00e3e6e706b392aa0d0
Kevin Roice <roice@ualberta.ca>
2024-05-15 18:35:38 -0600

    feat: feat ext for climbing images

--- discovery/utils/feat_extractors.py
+++ discovery/utils/feat_extractors.py
@@ -40,7 +40,9 @@ class MinigridFeaturesExtractor(BaseFeaturesExtractor):
         last_layer_activation="relu",
     ) -> None:
         super().__init__(observation_space, features_dim)
-        n_input_channels = observation_space["image"].shape[0]
+        if isinstance(observation_space, spaces.Dict):
+            observation_space = observation_space["image"]
+        n_input_channels = observation_space.shape[0]
         self.cnn = nn.Sequential(
             nn.Conv2d(n_input_channels, 16, (2, 2)),
             nn.ReLU(),
@@ -54,7 +56,7 @@ class MinigridFeaturesExtractor(BaseFeaturesExtractor):
         # Compute shape by doing one forward pass
         with torch.no_grad():
             n_flatten = self.cnn(
-                torch.as_tensor(observation_space["image"].sample()[None]).float()
+                torch.as_tensor(observation_space.sample()[None]).float()
             ).shape[1]
 
         if last_layer_activation == "relu":
