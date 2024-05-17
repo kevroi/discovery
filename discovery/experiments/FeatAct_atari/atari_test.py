c5df1717c9b39b0b9628b237377aa9e3ac871c72
Kevin Roice <roice@ualberta.ca>
2024-05-17 14:44:03 -0600

    feat: model saving works, need to do loading

--- discovery/experiments/FeatAct_atari/atari_test.py
+++ discovery/experiments/FeatAct_atari/atari_test.py
@@ -3,25 +3,29 @@ from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3 import PPO
 import gymnasium as gym
 import wandb
+from discovery.utils.save_callback import SnapshotCallback
 
 print("modules loaded")
 
 # There already exists an environment generator
 # that will make and wrap atari environments correctly.
 # Here we are also multi-worker training (n_envs=4 => 4 environments)
-vec_env = make_atari_env("ALE/MsPacman-v5", n_envs=4, seed=0)
+vec_env = make_atari_env("ALE/Seaquest-v5", n_envs=1, seed=0)
 # Frame-stacking with 4 frames
-vec_env = VecFrameStack(vec_env, n_stack=4)
+# vec_env = VecFrameStack(vec_env, n_stack=4)
 print("made env")
+
+log_directory = "discovery/experiments/FeatAct_atari/models/test"
+snapshot_callback = SnapshotCallback(check_freq=1_000, log_dir=log_directory, verbose=1)
 
 model = PPO("CnnPolicy", vec_env, verbose=1)
 print("made model")
-model.learn(total_timesteps=25_000)
+model.learn(total_timesteps=10_000, callback=snapshot_callback)
 print("model trained")
 
-obs = vec_env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=False)
-    obs, rewards, dones, info = vec_env.step(action)
-    print(rewards)
-    # vec_env.render("human")
+# obs = vec_env.reset()
+# while True:
+#     action, _states = model.predict(obs, deterministic=False)
+#     obs, rewards, dones, info = vec_env.step(action)
+#     print(rewards)
+#     # vec_env.render("human")
--- discovery/utils/save_callback.py
+++ discovery/utils/save_callback.py
@@ -2,86 +2,36 @@ import os
 from stable_baselines3.common.callbacks import BaseCallback
 
 
-class SaveOnBestTrainingRewardCallback(BaseCallback):
+class SnapshotCallback(BaseCallback):
     """
-    Callback for saving a model (the check is done every ``check_freq`` steps)
-    based on the training reward (in practice, we recommend using ``EvalCallback``).
+    Callback for saving a model (the check is done every ``check_freq`` steps).
 
     :param check_freq:
     :param log_dir: Path to the folder where the model will be saved.
       It must contains the file created by the ``Monitor`` wrapper.
     :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
     """
+
     def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
         super().__init__(verbose)
         self.check_freq = check_freq
         self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, "best_model")
-        self.best_mean_reward = -np.inf
 
     def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
+        if self.log_dir is not None:
+            os.makedirs(self.log_dir, exist_ok=True)
 
     def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
-            print(f"Saving new best model to {self.save_path}")
+            self.save_path = os.path.join(
+                self.log_dir, f"model_snapshot_{self.num_timesteps}_steps"
+            )  # self.num_timesteps = n_envs * n times env.step() was called
+            print(f"Saving new model to {self.save_path}")
             self.model.save(self.save_path)
 
         return True
 
-# class SaveModelCallback(BaseCallback):
-
-
-# class SaveModel:
-
-#     def __init__(
-#         self,
-#         model,
-#         save_path: str,
-#         name_prefix: str = "rl_model",
-#         verbose: int = 0,
-#     ):
-#         self.model = model
-#         self.save_path = save_path
-#         self.name_prefix = name_prefix
-
-#     def _init_callback(self) -> None:
-#         # Create folder if needed
-#         if self.save_path is not None:
-#             os.makedirs(self.save_path, exist_ok=True)
-
-#     def _checkpoint_path(self, checkpoint_type: str = "", extension: str = "") -> str:
-#         """
-#         Helper to get checkpoint path for each type of checkpoint.
-
-#         :param checkpoint_type: empty for the model, "replay_buffer_"
-#             or "vecnormalize_" for the other checkpoints.
-#         :param extension: Checkpoint file extension (zip for model, pkl for others)
-#         :return: Path to the checkpoint
-#         """
-#         return os.path.join(self.save_path, f"{self.name_prefix}_{checkpoint_type}{self.num_timesteps}_steps.{extension}")
-
-#     def __call__(self):
-#         """Save the model"""
-
-# class EveryNTimesteps(EventCallback):
-#     """
-#     Trigger a callback every ``n_steps`` timesteps
-
-#     :param n_steps: Number of timesteps between two trigger.
-#     :param callback: Callback that will be called
-#         when the event is triggered.
-#     """
-
-#     def __init__(self, n_steps: int, callback: BaseCallback):
-#         super().__init__(callback)
-#         self.n_steps = n_steps
-#         self.last_time_trigger = 0
-
-#     def _on_step(self) -> bool:
-#         if (self.num_timesteps - self.last_time_trigger) >= self.n_steps:
-#             self.last_time_trigger = self.num_timesteps
-#             return self._on_event()
-#         return True
+    def _on_training_end(self) -> None:
+        self.save_path = os.path.join(self.log_dir, f"model_snapshot_after_training")
+        print(f"Saving final model to {self.save_path}")
+        self.model.save(self.save_path)
