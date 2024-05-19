f7d312427d1d55f654790467e92cf133f8520f9a
David Szepesvari <szepesvari.david@gmail.com>
2024-05-18 22:41:50 -0600

    Merge.

--- discovery/experiments/FeatAct_atari/run_atari.py
+++ discovery/experiments/FeatAct_atari/run_atari.py
@@ -135,13 +135,15 @@ def main(args):
 
     log_directory = f"discovery/experiments/FeatAct_atari/models/dir_{hparam_yaml['learner']}_{hparam_yaml['env_name']}_{run_id}"
     snapshot_callback = SnapshotCallback(
-        check_freq=100_000 // hparam_yaml["n_envs"], log_dir=log_directory, verbose=1
+        check_freq=500_000 // hparam_yaml["n_envs"], log_dir=log_directory, verbose=1
     )
 
     print(
         f"Training {hparam_yaml['learner']} on {hparam_yaml['env_name']} with {hparam_yaml['feat_dim']} features."
     )
-    model.learn(total_timesteps=hparam_yaml["timesteps"], callback=snapshot_callback)
+    model.learn(
+        total_timesteps=hparam_yaml["timesteps"], callback=snapshot_callback
+    )
     model.save(
         f"discovery/experiments/FeatAct_atari/models/{hparam_yaml['learner']}_{hparam_yaml['env_name']}_{run_id}"
     )
