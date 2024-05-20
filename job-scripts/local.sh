91101d5e1ce2ec54cf92a63fd6307427d2e45d76
David Szepesvari <szepesvari.david@gmail.com>
2024-05-20 12:29:48 -0600

    Add instructions to bash script.

--- job-scripts/local.sh
+++ job-scripts/local.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# When starting over ssh, use tmux, otherwise the jobs will stop
+# when the terminal is closed.
+
 # Loop 50 times
 for i in {1..10}
 do
-    set -m; python3 discovery/experiments/FeatAct_minigrid/run_minigrid.py --device="cuda:2" --use_wandb &
+    python3 discovery/experiments/FeatAct_minigrid/run_minigrid.py --device="cuda:2" --use_wandb &
 done
