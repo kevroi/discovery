1d92b6ceafb8f0991c0c7a69d5977ee393d12b35
David Szepesvari <szepesvari.david@gmail.com>
2024-05-19 20:11:33 -0600

    Local launch specifies device.

--- job-scripts/local.sh
+++ job-scripts/local.sh
@@ -3,5 +3,5 @@
 # Loop 50 times
 for i in {1..10}
 do
-    set -m; python3 discovery/experiments/FeatAct_minigrid/run_minigrid.py --use_wandb &
+    set -m; python3 discovery/experiments/FeatAct_minigrid/run_minigrid.py --device="cuda:2" --use_wandb &
 done
