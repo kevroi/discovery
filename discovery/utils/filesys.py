71e429a0c23ab092f54161c40ca563aba67f53ec
David Szepesvari <szepesvari.david@gmail.com>
2024-05-20 17:05:42 -0600

    Improvements to filesys utils.

--- discovery/utils/filesys.py
+++ discovery/utils/filesys.py
@@ -4,19 +4,27 @@ import os
 PROJECT_DIR_NAME = "discovery"
 
 
-def find_root_directory(path: str) -> str:
+def find_root_directory(path: Optional[str] = None) -> str:
     """Finds the subpath to the root dir of the project in `path`."""
+    if path is None:
+        path = os.getcwd()
     where = path.find(PROJECT_DIR_NAME)
     if where == -1:
         raise ValueError(f"Cannot find the root directory of the project in '{path}'.")
     return path[: where + len(PROJECT_DIR_NAME)]
 
 
+def make_abs_path_in_root(rel_path: str) -> str:
+    """Returns the absolute path to the file or directory in the project root."""
+    root = find_root_directory()
+    return os.path.join(root, rel_path)
+
+
 def set_directory_in_project(
     rel_path: Optional[str] = None, create_dirs: bool = False
 ) -> str:
     """Changes the working dir to rel_path from the root of the project."""
-    path = find_root_directory(os.getcwd())
+    path = find_root_directory()
     if rel_path is not None:
         path = os.path.join(path, rel_path)
     if create_dirs:
