eb28da1c10088a542d8c0a378df360e5683f06bc
Kevin Roice <roice@ualberta.ca>
2024-06-22 13:19:13 -0600

    feat: added atari visualiser

--- discovery/experiments/FeatAct_atari/atari_test.py
+++ discovery/experiments/FeatAct_atari/atari_test.py
@@ -15,17 +15,21 @@ vec_env = make_atari_env("ALE/Seaquest-v5", n_envs=1, seed=0)
 # vec_env = VecFrameStack(vec_env, n_stack=4)
 print("made env")
 
-log_directory = "discovery/experiments/FeatAct_atari/models/test"
+log_directory = "discovery/experiments/FeatAct_atari/models"
 snapshot_callback = SnapshotCallback(check_freq=1_000, log_dir=log_directory, verbose=1)
 
-model = PPO("CnnPolicy", vec_env, verbose=1)
-print("made model")
-model.learn(total_timesteps=10_000, callback=snapshot_callback)
-print("model trained")
+# model = PPO("CnnPolicy", vec_env, verbose=1)
+# print("made model")
+# model.learn(total_timesteps=10_000, callback=snapshot_callback)
+# print("model trained")
 
-# obs = vec_env.reset()
-# while True:
-#     action, _states = model.predict(obs, deterministic=False)
-#     obs, rewards, dones, info = vec_env.step(action)
-#     print(rewards)
-#     # vec_env.render("human")
+
+model2 = PPO.load(
+    "/Users/kevinroice/Documents/research/discovery/discovery/experiments/FeatAct_atari/models/test/model_snapshot_9000_steps.zip"
+)
+obs = vec_env.reset()
+while True:
+    action, _states = model2.predict(obs, deterministic=False)
+    obs, rewards, dones, info = vec_env.step(action)
+    print(rewards)
+    vec_env.render("human")
